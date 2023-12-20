bf12aaadb5e717874260ea482d51235412dc3c47
Nafypy <Nafypy9@gmail.com>
2023-12-20 06:13:50 -0800

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that print a line to a standard error
+ * main - A program that print a line to  standard error
  * Return: 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19/n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19/n", stderr);
 	return (1);
 }
