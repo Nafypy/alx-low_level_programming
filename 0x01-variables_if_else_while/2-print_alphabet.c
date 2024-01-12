99d930c7d548336db9c97d07cf000319d8e0590f
Nafypy <Nafypy9@gmail.com>
2024-01-12 03:43:40 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char C;
+
+	C = 'a';
+	while (C <= 'z')
+	{
+		putchar('C');
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
