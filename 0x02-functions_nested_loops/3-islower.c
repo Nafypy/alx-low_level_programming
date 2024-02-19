3e65e560c2c401175316bd79aaac7c1995429d97
Nafypy <Nafypy9@gmail.com>
2024-02-18 23:02:30 -0800

    lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - check if a character is lowercase
+ * @c - character to be checked
+ * Return: 1 if c lowercase , 0 otherwise
+ */
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+		return (0);
+	}
+
