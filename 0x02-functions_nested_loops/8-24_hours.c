8a4ce1ff4d0c24987d170075aee7587aaaae4df3
Nafypy <Nafypy9@gmail.com>
2024-02-20 09:06:48 -0800

    Jack time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 23; a++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 5; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
